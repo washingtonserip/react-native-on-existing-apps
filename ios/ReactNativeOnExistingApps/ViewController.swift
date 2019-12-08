639cfdf24837107dc880c9f7a9c47474f03d4863
Washingto Pires <4washingtonbr@gmail.com>
2019-12-08 19:47:44 -0300

    Create iOS project

---
+++ ios/ReactNativeOnExistingApps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReactNativeOnExistingApps
+//
+//  Created by Washington Pires on 08/12/19.
+//  Copyright © 2019 Washington Pires. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
