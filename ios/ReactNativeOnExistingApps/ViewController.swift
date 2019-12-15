f832e72752434798d396da3a5ed8b20f617909bf
Washingto Pires <4washingtonbr@gmail.com>
2019-12-15 18:55:10 -0300

    React Native setup in iOS project

--- ios/ReactNativeOnExistingApps/ViewController.swift
+++ ios/ReactNativeOnExistingApps/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import React
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,28 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+     @IBAction func ReactButtonTapped(sender : UIButton) {
+        //Code to get the url of the main.jsbundle in the project.
+        guard let jsBundleLocation = Bundle.main.url(forResource: "main", withExtension: "jsbundle") else { return }
+        
+        //The data is used as initialProperties to React Native App.
+        let data:NSDictionary = [:]; //We can use this parameter to pass the data to the React native App from the Native App.
+        
+        //The RCTRootView is a native view used to host React-managed views within the app. Can be used just like any ordinary UIView.
+        let rootView = RCTRootView(
+            bundleURL: jsBundleLocation,
+            moduleName: "ReactNativeOnExistingApps",
+            initialProperties: data as [NSObject : AnyObject],
+            launchOptions: nil
+        )
+        
+        let viewController = UIViewController()
+
+        viewController.view = rootView
+        
+        self.present(viewController, animated: true, completion: nil)
+
+    }
 
 }
 
