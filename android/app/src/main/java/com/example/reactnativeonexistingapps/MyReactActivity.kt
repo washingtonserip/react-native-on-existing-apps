6aeb00b0daac33e3c655dbf0de08ad17841ec7d2
Washingto Pires <4washingtonbr@gmail.com>
2020-01-13 00:29:16 -0300

    React Native setup in Android project

---
+++ android/app/src/main/java/com/example/reactnativeonexistingapps/MyReactActivity.kt
@@ -0,0 +1,80 @@
+package com.example.reactnativeonexistingapps
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.KeyEvent
+import com.facebook.react.BuildConfig
+import com.facebook.react.ReactInstanceManager
+import com.facebook.react.ReactRootView
+import com.facebook.react.common.LifecycleState
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
+import com.facebook.react.shell.MainReactPackage
+
+class MyReactActivity : Activity(), DefaultHardwareBackBtnHandler {
+    private var mReactRootView: ReactRootView? = null
+    private var mReactInstanceManager: ReactInstanceManager? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mReactRootView = ReactRootView(this)
+        mReactInstanceManager = ReactInstanceManager.builder()
+            .setApplication(application)
+            .setCurrentActivity(this)
+            .setBundleAssetName("index.android.bundle")
+            .setJSMainModulePath("index")
+            .addPackage(MainReactPackage())
+            .setUseDeveloperSupport(BuildConfig.DEBUG)
+            .setInitialLifecycleState(LifecycleState.RESUMED)
+            .build()
+        mReactRootView!!.startReactApplication(mReactInstanceManager, "ReactNativeOnExistingApps", null)
+
+        setContentView(mReactRootView)
+    }
+
+    override fun invokeDefaultOnBackPressed() {
+        super.onBackPressed()
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager!!.onHostPause(this)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager!!.onHostResume(this, this)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager!!.onHostDestroy(this)
+        }
+        mReactRootView!!.unmountReactApplication()
+    }
+
+    override fun onBackPressed() {
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager!!.onBackPressed()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onKeyUp(keyCode: Int, event: KeyEvent): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_MENU && mReactInstanceManager != null) {
+            mReactInstanceManager!!.showDevOptionsDialog()
+            return true
+        }
+        return super.onKeyUp(keyCode, event)
+    }
+
+}
